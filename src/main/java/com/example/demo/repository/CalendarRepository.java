29c7635373335bc98dc02d42798bde9339b96438
https://github.com/myeongjaeking/algorithm-c- <https://github.com/myeongjaeking/algorithm-c->
2024-07-20 16:49:14 +0900

    - 알림 수정

--- src/main/java/com/example/demo/repository/CalendarRepository.java
+++ src/main/java/com/example/demo/repository/CalendarRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Calendar;
+import com.example.demo.domain.Routine;
+import com.example.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CalendarRepository extends JpaRepository<Calendar,Long> {
+    Routine findByUser(User user);
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findBySub(String sub);
 
     User findByEmail(String email);
-
+    List<User> findAll();
 }
