012795275a81c53f45fce53e55d8092d2efd26bf
myeongjaeking <112608799+myeongjaeking@users.noreply.github.com>
2024-07-28 13:18:38 +0900

    Update ToDo.java

--- src/main/java/com/example/demo/domain/ToDo.java
+++ src/main/java/com/example/demo/domain/ToDo.java
@@ -20,7 +20,60 @@ public class ToDo {
     @JoinColumn(name = "calendar_id")
     private Calendar calendar;
     @Column
+<<<<<<< HEAD
+    private boolean notificationIsTrue=true;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalTime getNotification() {
+        return notification;
+    }
+
+    public void setNotification(LocalTime notification) {
+        this.notification = notification;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    public boolean isNotificationIsTrue() {
+        return notificationIsTrue;
+    }
+
+    public void setNotificationIsTrue(boolean notificationIsTrue) {
+        this.notificationIsTrue = notificationIsTrue;
+    }
+
+    public Routine getRoutine() {
+        return routine;
+    }
+
+    public void setRoutine(Routine routine) {
+        this.routine = routine;
+    }
+
+=======
     private boolean notificationIsTrue;
+>>>>>>> origin/main
     @ManyToOne
     @JoinColumn(name = "routine_id")
     private Routine routine;
